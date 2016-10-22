68649f54ab40290268dd1f20ea18e06533f8e30f
Stephan Ahlf <stephan.ahlf@googlemail.com>
2016-10-22 10:23:54 +0200

    add demos and unit tests

--- test/index.js
+++ test/index.js
@@ -1,23 +1,18 @@
 "use strict";
 var fs = require("fs");
-var path = require("path");
+var should = require("should");
 var jsdom = require("jsdom");
+var Mask = require("../lib/");
+var testMasks = require("./testMasks.json");
 var Dom = function(done){
   return jsdom.env({
     html: fs.readFileSync("index.html"),
-    scripts: [
-      path.join(__dirname, "..", "lib/index.js")
-    ],
-    features: {
-     // FetchExternalResources: ["script"],
-     // ProcessExternalResources: ["script"]
-    },
     done: done
   });
 }
 var assert = require("assert");
-//var msk = require("../lib");
-/*var newModuleWrap = function(script) {
+/*
+  var newModuleWrap = function(script) {
 	var nodeParms = ["exports", "require", "module", "__filename", "__dirname", "process", "global"];
 	if (settings.nodeModule.arguments && isArray(settings.nodeModule.arguments)){
 		for (var i = 0; i < settings.nodeModule.arguments.length; i++) {
@@ -38,7 +33,7 @@ var assert = require("assert");
 	return w[0] + script + w[1];
 };
 
-var ModedModule = function (ironNodeSettings) {
+var ModdedModule = function (ironNodeSettings) {
 	settings = ironNodeSettings;
 	console.log(settings);
 	if (ironNodeSettings && ironNodeSettings.nodeModule){
@@ -51,12 +46,28 @@ var ModedModule = function (ironNodeSettings) {
 };*/
 
 describe("msk", function () {
-  it("should return a value", function (done) {
-    
-    var dom = new Dom(function(err, window) {
-      assert(err === null);
-      assert(true, window.mask.val() === "155.125.25.25");
+  it("should handle ip4 test mask", function (done) {
+    Dom(function(err, window) {
+      assert(err === null); 
+			var mask = new Mask("txt-ip4", testMasks.ip4, window.document);
+			mask.val("111.11.1.1").format();
+      mask.val().should.equal("<test>: 111.11.1.1 <testing ip mask...>");
+      mask.unmaskedValue().should.equal("111.11.1.1");
       done();
     });
   });
+
+
+  it("should handle currency test mask", function (done) {
+    Dom(function(err, window) {
+      assert(err === null); 
+			var mask = new Mask("txt-currency", testMasks.currency, window.document);
+			mask.val("111.222").format();
+      mask.val().should.equal("$ 111.222 <testing currency mask...>");
+      mask.unmaskedValue().should.equal("111.222");
+      done();
+    });
+  });
+
 });
+ 
